0271a339681ed339c904e545d91e52be00693a2f
Gregory A.S. Wright <geslim@gmail.com>
2020-06-30 20:36:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash
+  name_hash.each do |name, age|
+    if age < age.max(2)
+
+    end
+  end
 end
