5956c9516ec6e5075ca6c27c6aae8c34ceff3748
Gregory A.S. Wright <geslim@gmail.com>
2020-07-01 12:42:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,17 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  person = []
+
   name_hash.each do |name, number|
     if name[0] < name[1] && name[2]
-      person = name
+      name
     elsif name[1] < name[0] && name[2]
-      person = name
+      name
     elsif name[2] < name[0] && name[1]
-      person = name
+      name
     else
       nil
     end
   end
-  person
 end
