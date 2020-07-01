f12ee65c6d193749f26ff980b07c6ec2c4c667e6
Gregory A.S. Wright <geslim@gmail.com>
2020-07-01 14:15:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   person = nil
   min_num = nil
   name_hash.each do |name, number|
-    if number <
+    if number < number.length
       min_num = number
       person = name
     else
